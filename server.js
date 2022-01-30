aad7f7203715a15a1b18c037a2d4bf1da00aad37
juangom1950 <juangom73@gmail.com>
2022-01-30 17:17:58 -0500

    Managing Live Clusters With PM2

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//const cluster = require('cluster');
+//const os = require('os');
 
 const app = express();
 
@@ -10,10 +12,17 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
+  // This functions can slow performance
+  // Comverts an object into a string representation
+  //JSON.stringify({}) => "{}"
+  // It takes a string object and converted into a javascriipt object
+  //JSON.parse("{}") => {}
+  // [2,3,4,5,].sort()
   res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
+  // Our server can't do enything else until this delay func is executed.
   delay(4000);
   res.send(`Beep beep beep! ${process.pid}`);
 });
@@ -21,3 +30,20 @@ app.get('/timer', (req, res) => {
 console.log('Running server.js...');
 console.log('Worker process started.');
 app.listen(3000);
+
+// if (cluster.isMaster) {
+//   console.log('Master has been started...');
+//   // This is total number of cores in our serfer machine
+//   const NUMB_WORKERS = os.cpus().length;
+//   for (let i = 0; i < NUMB_WORKERS; i++) {
+//     cluster.fork();
+//   }
+//   // Create a cluster. Here we are creating 2
+//   // We can create as many as we want
+//   // Here we can only handle two processes at the time.
+//   //cluster.fork();
+//   //cluster.fork();
+// } else {
+//   console.log('Worker process started.');
+//   app.listen(3000);
+// }
